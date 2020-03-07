975cb8e5b048ccff08ee241302cf7e7981cea046
Jan Benda <jan.benda@uni-tuebingen.de>
2020-03-07 10:00:49 +0100

    new colors module

--- plottools/plotformat.py
+++ plottools/plotformat.py
@@ -7,15 +7,6 @@ Layout settings for a plot figure.
 - `cm_size()`: convert dimensions from cm to inch.
 - `adjust_fs()`: compute plot margins from multiples of the current font size.
 
-Dictionaries with colors:
-- `colors`: the default colors, set to one of the following:
-- `colors_bendalab`: muted colors used by the Benda-lab.
-- `colors_bendalab_vivid`: vivid colors used by the Benda-lab.
-- `colors_plain`: plain rgb colors.
-- `colors_henninger`: colors by Joerg Henninger.
-- `colors_scicomp`: colors from the scientific computing script.
-- `colors_uni_tuebingen`: colors of the corporate design of the university of Tuebingen.
-
 Line and marker styles.
 """
 
@@ -23,6 +14,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from collections import OrderedDict
+from .colors import colors
 from .spines import show_spines, set_spines_outward, set_spines_bounds, set_default_spines
 from .ticks import set_xticks_delta, set_yticks_delta, set_xticks_none, set_yticks_none
 from .ticks import set_xticks_format, set_yticks_format, set_xticks_blank, set_yticks_blank
@@ -31,96 +23,6 @@ from .insets import inset, zoomed_inset
 from .labelaxes import label_axes
 from .scalebars import xscalebar, yscalebar, scalebars
 from .significance import significance_bar
-
-
-""" Muted colors used by the Benda-lab. """
-colors_bendalab = OrderedDict()
-colors_bendalab['red'] = '#C02717'
-colors_bendalab['orange'] = '#F78017'
-colors_bendalab['yellow'] = '#F0D730'
-colors_bendalab['green'] = '#AAB71B'
-colors_bendalab['darkgreen'] = '#007030'
-colors_bendalab['cyan'] = '#40A787'
-colors_bendalab['blue'] = '#2060A7'
-colors_bendalab['purple'] = '#53379B'
-colors_bendalab['magenta'] = '#873770'
-colors_bendalab['pink'] = '#D03050'
-colors_bendalab['white'] = '#FFFFFF'
-colors_bendalab['gray'] = '#A0A0A0'
-colors_bendalab['black'] = '#000000'
-
-
-""" Vivid colors used by the Benda-lab. """
-colors_bendalab_vivid = OrderedDict()
-colors_bendalab_vivid['red'] = '#D71000'
-colors_bendalab_vivid['orange'] = '#FF9000'
-colors_bendalab_vivid['yellow'] = '#FFF700'
-colors_bendalab_vivid['green'] = '#30D700'
-colors_bendalab_vivid['darkgreen'] = '#008020'
-colors_bendalab_vivid['cyan'] = '#00F0B0'
-colors_bendalab_vivid['blue'] = '#0020C0'
-colors_bendalab_vivid['purple'] = '#8000C0'
-colors_bendalab_vivid['magenta'] = '#B000B0'
-colors_bendalab_vivid['pink'] = '#F00080'
-colors_bendalab_vivid['white'] = '#FFFFFF'
-colors_bendalab_vivid['gray'] = '#A7A7A7'
-colors_bendalab_vivid['black'] = '#000000'
-
-""" Plain rgb colors. """
-colors_plain = OrderedDict()
-colors_plain['red'] = '#FF0000'
-colors_plain['orange'] = '#FFA500'
-colors_plain['yellow'] = '#FFFF00'
-colors_plain['green'] = '#00FF00'
-colors_plain['darkgreen'] = '#008000'
-colors_plain['cyan'] = '#00FFFF'
-colors_plain['blue'] = '#0000FF'
-colors_plain['purple'] = '#8000FF'
-colors_plain['magenta'] = '#FF00FF'
-colors_plain['pink'] = '#FF0080'
-colors_plain['white'] = '#FFFFFF'
-colors_plain['gray'] = '#808080'
-colors_plain['black'] = '#000000'
-
-""" Colors by Joerg Henninger. """
-colors_henninger = OrderedDict()
-colors_henninger['red'] = '#BA2D22'
-colors_henninger['orange'] = '#F47F17'
-colors_henninger['green'] = '#AAB71B'
-colors_henninger['blue'] = '#3673A4'
-colors_henninger['purple'] = '#53379B'
-
-""" Colors from the scientific computing script. """
-colors_scicomp = OrderedDict()
-colors_scicomp['red'] = '#CC0000'
-colors_scicomp['orange'] = '#FF9900'
-colors_scicomp['lightorange'] = '#FFCC00'
-colors_scicomp['yellow'] = '#FFFF66'
-colors_scicomp['green'] = '#99FF00'
-colors_scicomp['blue'] = '#0000CC'
-
-""" Colors of the corporate design of the university of Tuebingen.
-The first three are the primary colors, the remaining ones the secondary colors.
-"""
-colors_uni_tuebingen = OrderedDict()
-colors_uni_tuebingen['red'] = '#A51E37'
-colors_uni_tuebingen['gold'] = '#B4A069'
-colors_uni_tuebingen['black'] = '#32414B'
-colors_uni_tuebingen['darkblue'] = '#415A8C'
-colors_uni_tuebingen['blue'] = '#0069AA'
-colors_uni_tuebingen['lightblue'] = '#50AAC8'
-colors_uni_tuebingen['cyan'] = '#82B9A0'
-colors_uni_tuebingen['green'] = '#7DA54B'
-colors_uni_tuebingen['darkgreen'] = '#326E1E'
-colors_uni_tuebingen['lightred'] = '#C8503C'
-colors_uni_tuebingen['magenta'] = '#AF6E96'
-colors_uni_tuebingen['gray'] = '#B4A096'
-colors_uni_tuebingen['lightorange'] = '#D7B469'
-colors_uni_tuebingen['orange'] = '#D29600'
-colors_uni_tuebingen['brown'] = '#916946'
-
-""" Default color palette. """
-colors = colors_bendalab
 
 
 """ line styles for plot(). """
@@ -500,178 +402,6 @@ def adjust_fs(fig=None, left=5.5, right=0.5, bottom=2.8, top=0.5):
              'top': 1.0 - top*fs/h }
 
 
-def lighter(color, lightness):
-    """ Make a color lighter.
-
-    Parameters
-    ----------
-    color: string
-        An RGB color as a hexadecimal string (e.g. '#rrggbb').
-    lightness: float
-        The smaller the lightness, the lighter the returned color.
-        A lightness of 1 leaves the color untouched.
-        A lightness of 0 returns white.
-
-    Returns
-    -------
-    color: string
-        The lighter color as a hexadecimal RGB string (e.g. '#rrggbb').
-    """
-    r = int(color[1:3], 16)
-    g = int(color[3:5], 16)
-    b = int(color[5:7], 16)
-    rl = r + (1.0-lightness)*(0xff - r)
-    gl = g + (1.0-lightness)*(0xff - g)
-    bl = b + (1.0-lightness)*(0xff - b)
-    return '#%02X%02X%02X' % (rl, gl, bl)
-
-
-def darker(color, saturation):
-    """ Make a color darker.
-
-    Parameters
-    ----------
-    color: string
-        An RGB color as a hexadecimal string (e.g. '#rrggbb').
-    saturation: float
-        The smaller the saturation, the darker the returned color.
-        A saturation of 1 leaves the color untouched.
-        A saturation of 0 returns black.
-
-    Returns
-    -------
-    color: string
-        The darker color as a hexadecimal RGB string (e.g. '#rrggbb').
-    """
-    r = int(color[1:3], 16)
-    g = int(color[3:5], 16)
-    b = int(color[5:7], 16)
-    rd = r * saturation
-    gd = g * saturation
-    bd = b * saturation
-    return '#%02X%02X%02X' % (rd, gd, bd)
-
-
-def plot_colors(ax, colors, n=1):
-    """ Plot all colors of a palette and optionally some lighter and darker variants.
-
-    Parameters
-    ----------
-    ax: matplotlib axes
-        Subplot to use for plotting the colors.
-    colors: dict
-        A dictionary with names and rgb hex-strings of colors.
-    n: int
-        If one, plot the colors of the palette only.
-        If larger than one, plot in addition that many
-        lighter and darker versions of the colors.
-    """
-    if n < 1:
-        n = 1
-    nn = 1 + 2*(n-1)
-    rectx = np.array([0.0, 1.0, 1.0, 0.0, 0.0])
-    recty = np.array([0.0, 0.0, 1.0, 1.0, 0.0])
-    if n > 1:
-        recty *= 0.9
-    for k, c in enumerate(colors):
-        for i in range(-n+1, n):
-            if i < 0:
-                ax.fill(rectx + 1.5*k, (i+n-1+recty)/nn, color=darker(colors[c], (n+i)/float(n)))
-            elif i > 0:
-                ax.fill(rectx + 1.5*k, (i+n-1+recty)/nn, color=lighter(colors[c], (n-i)/float(n)))
-            else:
-                ax.fill(rectx + 1.5*k, (i+n-1+recty)/nn, color=colors[c])
-        ax.text(0.5 + 1.5*k, -0.09, c, ha='center')
-        ax.text(0.5 + 1.5*k, -0.16, colors[c], ha='center')
-    if n > 1:
-        for i in range(-n+1, n):
-            if i < 0:
-                ax.text(-0.1, (i+n-0.6)/nn, '%.0f%%' % (100.0*(n+i)/float(n)), ha='right')
-            elif i > 0:
-                ax.text(-0.1, (i+n-0.6)/nn, '%.0f%%' % (100.0*(n-i)/float(n)), ha='right')
-            else:
-                ax.text(-0.1, (i+n-0.6)/nn, '100%', ha='right')
-        ax.text(-1.1, 0.75, 'lighter', ha='center', va='center', rotation='vertical')
-        ax.text(-1.1, 0.25, 'darker', ha='center', va='center', rotation='vertical')
-        ax.set_xlim(-1.5, len(colors)*1.5)
-    else:
-        ax.set_xlim(-0.5, len(colors)*1.5)
-    ax.set_ylim(-0.2, 1.05)
-
-
-def plot_complementary_colors(ax, colors):
-    """ Plot complementary colors of a palette on top of each other.
-
-    Parameters
-    ----------
-    ax: matplotlib axes
-        Subplot to use for plotting the colors.
-    colors: dict
-        A dictionary with names and rgb hex-strings of colors.
-    """
-    rectx = np.array([0.0, 1.0, 1.0, 0.0, 0.0])
-    recty = np.array([0.0, 0.0, 1.0, 1.0, 0.0])
-    n = 0
-    if 'red' in colors and 'green' in colors:
-        ax.fill(rectx + 1.5*n, recty + 1.0, color=colors['red'])
-        ax.fill(rectx + 1.5*n, recty + 0.0, color=colors['green'])
-        n += 1
-    if 'orange' in colors and 'blue' in colors:
-        ax.fill(rectx + 1.5*n, recty + 1.0, color=colors['orange'])
-        ax.fill(rectx + 1.5*n, recty + 0.0, color=colors['blue'])
-        n += 1
-    if 'yellow' in colors and 'magenta' in colors:
-        ax.fill(rectx + 1.5*n, recty + 1.0, color=colors['yellow'])
-        ax.fill(rectx + 1.5*n, recty + 0.0, color=colors['magenta'])
-        n += 1
-    if 'pink' in colors and 'cyan' in colors:
-        ax.fill(rectx + 1.5*n, recty + 1.0, color=colors['pink'])
-        ax.fill(rectx + 1.5*n, recty + 0.0, color=colors['cyan'])
-        n += 1
-    if 'pink' in colors and 'blue' in colors:
-        ax.fill(rectx + 1.5*n, recty + 1.0, color=colors['blue'])
-        ax.fill(rectx + 1.5*n, recty + 0.0, color=colors['pink'])
-        n += 1
-    ax.set_xlim(-0.5, n*1.5)
-    ax.set_ylim(-0.1, 2.1)
-
-
-def plot_color_comparison(ax, colorsa, *args):
-    """ Plot matching colors of severals palettes on top of each other.
-
-    Parameters
-    ----------
-    ax: matplotlib axes
-        Subplot to use for plotting the colors.
-    colorsa: dict or tuple (dict, string)
-        A dictionary with names and rgb hex-strings of colors.
-        This is the reference palette which is plotted completely at the bottom.
-        The optional second name is used as a string to annotated the colors.
-    args: list of dicts or tuples (dict, string)
-        Further dictionaries with names and rgb hex-strings of colors.
-        Colors with names matching the ones from `colorsa` are plotted on top.
-        The optional second element is used as a string to annotated the colors.
-    """
-    rectx = np.array([0.0, 1.0, 1.0, 0.0, 0.0])
-    recty = np.array([0.0, 0.0, 1.0, 1.0, 0.0])
-    if isinstance(colorsa, (list, tuple)):
-        ax.text(-0.1, 0.5, colorsa[1], rotation='vertical', ha='right', va='center')
-        colorsa = colorsa[0]
-    for k, c in enumerate(colorsa):
-        ax.fill(rectx + 1.5*k, recty + 0.0, color=colorsa[c])
-        for i, cbn in enumerate(args):
-            cb = cbn
-            if isinstance(cbn, (list, tuple)):
-                cb = cbn[0]
-                if k == 0:
-                    ax.text(-0.1, 1.5+i, cbn[1], rotation='vertical', ha='right', va='center')
-            if c in cb:
-                ax.fill(rectx + 1.5*k, recty + 1 + i, color=cb[c])
-        ax.text(0.5 + 1.5*k, -0.1, c, ha='center')
-    ax.set_xlim(-0.5, len(colorsa)*1.5)
-    ax.set_ylim(-0.2, 1.1 + len(args))
-
-
 def plot_linestyles(ax):
     """ Plot names and lines of all available line styles.
 
@@ -732,58 +462,37 @@ def plot_linepointstyles(ax):
     ax.set_title('linepoint styles')
         
 
-def demo(mode=1):
+def demo(mode='line'):
     """ Run a demonstration of the plotformat module.
 
     Parameters
     ----------
-    mode: int or string
-        1: plot the default color palette
-        n>1: plot the default color palette with n-1 lighter and darker colors
+    mode: string
         'linestyles': plot the names and lines of all available line styles
         'pointstyles': plot the names and points (markers) of all available point styles
         'linepointstyles': plot the names and lines of all available linepoint styles
-        'complementary': plot complementary colors of the default color palette
-        'comparison': plot the default color palette in comparison with colors_bendalab_vivid
     """
     # set default plot parameter:
     plot_format(spines='')
     # figsize in centimeter:
     fig, ax = plt.subplots(figsize=cm_size(16.0, 10.0))
     fig.subplots_adjust(**adjust_fs(fig, left=1.0, bottom=1.0, top=1.0, right=1.0))
-    # colors and linestyles:
-    if isinstance(mode, int):
-        plot_colors(ax, colors, mode)
-        ax.set_ylim(-0.27, 1.05)
+    if 'linep' in mode:
+        plot_linepointstyles(ax)
+    elif 'line' in mode:
+        plot_linestyles(ax)
+    elif 'point' in mode:
+        plot_pointstyles(ax)
     else:
-        if 'linep' in mode:
-            plot_linepointstyles(ax)
-        elif 'line' in mode:
-            plot_linestyles(ax)
-        elif 'point' in mode:
-            plot_pointstyles(ax)
-        elif 'compl' in mode:
-            plot_complementary_colors(ax, colors)
-        elif 'compa' in mode:
-            plot_color_comparison(ax, (colors_bendalab, 'benda_lab'),
-                                  (colors_bendalab_vivid, 'bendalab_vivid'),
-                                  (colors_plain, 'plain'),
-                                  (colors_henninger, 'henninger'),
-                                  (colors_scicomp, 'scicomp'),
-                                  (colors_uni_tuebingen, 'uni_tuebingen'))
-        elif 'henn' in mode:
-            plot_color_comparison(ax, (colors_bendalab, 'benda_lab'),
-                                  (colors_henninger, 'henninger'))
-        else:
-            print('unknown option %s!' % mode)
+        print('unknown option %s!' % mode)
+        print('possible options are: line, point, linep(oints)')
+        return
     plt.show()
 
 
 if __name__ == "__main__":
     import sys
-    mode = 1
+    mode = 'line'
     if len(sys.argv) > 1:
         mode = sys.argv[1]
-        if mode.isdigit():
-            mode = int(mode)
     demo(mode)
