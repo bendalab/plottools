2394d3284a9de4a612a45593f14a7f7e24dccbac
Jan Benda <jan.benda@uni-tuebingen.de>
2020-03-20 15:41:08 +0100

    [ticks] renamed set_xticks_none() to set_xticks_off()

--- plottools/ticks.py
+++ plottools/ticks.py
@@ -8,15 +8,15 @@ The following functions are also added as members to mpl.axes.Axes:
 - `set_yticks_delta()`: set distance between yticks.
 - `set_xticks_fixed()`: set custom xticks at fixed positions.
 - `set_yticks_fixed()`: set custom yticks at fixed positions.
-- `set_xticks_none()`: do not draw any xticks.
-- `set_yticks_none()`: do not draw any yticks.
+- `set_xticks_off()`: do not draw any xticks.
+- `set_yticks_off()`: do not draw any yticks.
 - `set_xticks_format()`: format xticks according to formatter string.
 - `set_yticks_format()`: format yticks according to formatter string.
 - `set_xticks_blank()`: draw xticks without labeling them.
 - `set_yticks_blank()`: draw yticks without labeling them.
 
-- `set_minor_xticks_none()`: do not draw any minor xticks.
-- `set_minor_yticks_none()`: do not draw any minor yticks.
+- `set_minor_xticks_off()`: do not draw any minor xticks.
+- `set_minor_yticks_off()`: do not draw any minor yticks.
 
 """
 
@@ -92,7 +92,7 @@ def set_yticks_fixed(ax, locs, labels='%g'):
         ax.yaxis.set_major_formatter(ticker.FormatStrFormatter(labels))
 
 
-def set_xticks_none(ax):
+def set_xticks_off(ax):
     """ Do not draw any xticks.
 
     Parameters
@@ -103,7 +103,7 @@ def set_xticks_none(ax):
     ax.xaxis.set_major_locator(ticker.NullLocator())
 
 
-def set_yticks_none(ax):
+def set_yticks_off(ax):
     """ Do not draw any yticks.
 
     Parameters
@@ -162,7 +162,7 @@ def set_yticks_blank(ax):
     ax.yaxis.set_major_formatter(ticker.NullFormatter())
 
 
-def set_minor_xticks_none(ax):
+def set_minor_xticks_off(ax):
     """ Do not draw any minor xticks.
 
     Parameters
@@ -173,7 +173,7 @@ def set_minor_xticks_none(ax):
     ax.xaxis.set_minor_locator(ticker.NullLocator())
 
 
-def set_minor_yticks_none(ax):
+def set_minor_yticks_off(ax):
     """ Do not draw any minor yticks.
 
     Parameters
@@ -189,14 +189,14 @@ mpl.axes.Axes.set_xticks_delta = set_xticks_delta
 mpl.axes.Axes.set_yticks_delta = set_yticks_delta
 mpl.axes.Axes.set_xticks_fixed = set_xticks_fixed
 mpl.axes.Axes.set_yticks_fixed = set_yticks_fixed
-mpl.axes.Axes.set_xticks_none = set_xticks_none
-mpl.axes.Axes.set_yticks_none = set_yticks_none
+mpl.axes.Axes.set_xticks_off = set_xticks_off
+mpl.axes.Axes.set_yticks_off = set_yticks_off
 mpl.axes.Axes.set_xticks_format = set_xticks_format
 mpl.axes.Axes.set_yticks_format = set_yticks_format
 mpl.axes.Axes.set_xticks_blank = set_xticks_blank
 mpl.axes.Axes.set_yticks_blank = set_yticks_blank
-mpl.axes.Axes.set_minor_xticks_none = set_minor_xticks_none
-mpl.axes.Axes.set_minor_yticks_none = set_minor_yticks_none
+mpl.axes.Axes.set_minor_xticks_off = set_minor_xticks_off
+mpl.axes.Axes.set_minor_yticks_off = set_minor_yticks_off
 
     
 def demo():
@@ -211,10 +211,10 @@ def demo():
     axs[0,0].set_xticks_delta(1.0)
     axs[0,0].set_yticks_delta(0.5)
 
-    axs[0,1].text(0.1, 0.8, 'ax.set_xticks_none()')
-    axs[0,1].text(0.1, 0.6, 'ax.set_yticks_none()')
-    axs[0,1].set_xticks_none()
-    axs[0,1].set_yticks_none()
+    axs[0,1].text(0.1, 0.8, 'ax.set_xticks_off()')
+    axs[0,1].text(0.1, 0.6, 'ax.set_yticks_off()')
+    axs[0,1].set_xticks_off()
+    axs[0,1].set_yticks_off()
 
     axs[1,0].text(0.1, 0.8, "ax.set_xticks_format('%04.1f')")
     axs[1,0].text(0.1, 0.6, "ax.set_yticks_format('%.2f')")
