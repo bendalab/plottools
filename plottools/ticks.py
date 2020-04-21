fcb785b0af49463378a3d787d468f66d04d60c7b
Jan Benda <jan.benda@uni-tuebingen.de>
2020-04-21 23:18:27 +0200

    [ticks] new common_xtick_labels()

--- plottools/ticks.py
+++ plottools/ticks.py
@@ -3,7 +3,7 @@
 
 Convience functions for setting tick locations and formats.
 
-The following functions are also added as members to mpl.axes.Axes:
+The following functions are available as members of mpl.axes.Axes:
 - `set_xticks_delta()`: set distance between xticks.
 - `set_yticks_delta()`: set distance between yticks.
 - `set_xticks_fixed()`: set custom xticks at fixed positions.
@@ -19,6 +19,10 @@ The following functions are also added as members to mpl.axes.Axes:
 
 - `set_minor_xticks_off()`: do not draw any minor xticks.
 - `set_minor_yticks_off()`: do not draw any minor yticks.
+
+The following functions are available as members of mpl.figure.Figure:
+- `common_xtick_labels()`: simplify common xtick labels.
+- `common_ytick_labels()`: simplify common ytick labels.
 
 """
 
@@ -229,6 +233,74 @@ def set_minor_yticks_off(ax):
     ax.yaxis.set_minor_locator(ticker.NullLocator())
 
 
+def common_xtick_labels(fig, axes=None):
+    """ Simplify common xtick labels.
+
+    Parameters
+    ----------
+    fig: matplotlib figure
+        The figure containing the axes.
+    axes: None or sequence of matplotlib axes
+        Axes whose xticks should be combined.
+        If None take all axes of the figure.
+
+    """
+    if axes is None:
+        axes = fig.get_axes()
+    coords = np.array([ax.get_position().get_points().ravel() for ax in axes])
+    # keep the xtick labels only at the lowest axes and center the common xlabel:
+    miny = np.min(coords[:,1])
+    minx = np.min(coords[:,0])
+    maxx = np.max(coords[:,2])
+    xl = 0.5*(minx+maxx)
+    done = False
+    for ax in axes:
+        if ax.get_position().p0[1] > miny + 1e-6:
+            ax.set_xlabel('')
+            ax.xaxis.set_major_formatter(ticker.NullFormatter())
+        elif done:
+            ax.set_xlabel('')
+        else:
+            x, y = ax.xaxis.get_label().get_position()
+            x = ax.transAxes.inverted().transform(fig.transFigure.transform((xl, 0)))[0]
+            ax.xaxis.get_label().set_position((x, y))
+            done = True
+
+
+def common_ytick_labels(fig, axes=None):
+    """ Simplify common ytick labels.
+
+    Parameters
+    ----------
+    fig: matplotlib figure
+        The figure containing the axes.
+    axes: None or sequence of matplotlib axes
+        Axes whose yticks should be combined.
+        If None take all axes of the figure.
+
+    """
+    if axes is None:
+        axes = fig.get_axes()
+    coords = np.array([ax.get_position().get_points().ravel() for ax in axes])
+    # keep the ytick labels only at the leftmost axes and center the common ylabel:
+    minx = np.min(coords[:,0])
+    miny = np.min(coords[:,1])
+    maxy = np.max(coords[:,3])
+    yl = 0.5*(miny+maxy)
+    done = False
+    for ax in axes:
+        if ax.get_position().p0[0] > minx + 1e-6:
+            ax.set_ylabel('')
+            ax.yaxis.set_major_formatter(ticker.NullFormatter())
+        elif done:
+            ax.set_ylabel('')
+        else:
+            x, y = ax.yaxis.get_label().get_position()
+            y = ax.transAxes.inverted().transform(fig.transFigure.transform((0, yl)))[1]
+            ax.yaxis.get_label().set_position((x, y))
+            done = True
+
+            
 # make functions available as member variables:
 mpl.axes.Axes.set_xticks_delta = set_xticks_delta
 mpl.axes.Axes.set_yticks_delta = set_yticks_delta
@@ -244,6 +316,8 @@ mpl.axes.Axes.set_xticks_blank = set_xticks_blank
 mpl.axes.Axes.set_yticks_blank = set_yticks_blank
 mpl.axes.Axes.set_minor_xticks_off = set_minor_xticks_off
 mpl.axes.Axes.set_minor_yticks_off = set_minor_yticks_off
+mpl.figure.Figure.common_xtick_labels = common_xtick_labels
+mpl.figure.Figure.common_ytick_labels = common_ytick_labels
 
     
 def demo():
@@ -286,6 +360,15 @@ def demo():
     axs[2,1].set_yscale('log')
     axs[2,1].set_ylim(1e0, 1e6)
     axs[2,1].set_yticks_prefix()
+
+    fig, axs = plt.subplots(2, 2)
+    for ax in axs.ravel():
+        ax.set_xlabel('xlabel')
+        ax.set_ylabel('ylabel')
+    axs[0,0].text(0.1, 0.8, 'fig.common_xtick_labels()')
+    axs[0,0].text(0.1, 0.6, 'fig.common_ytick_labels()')
+    fig.common_xtick_labels() 
+    fig.common_ytick_labels()
     
     plt.show()
 
