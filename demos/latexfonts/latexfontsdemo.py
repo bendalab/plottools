950fed12175c172a4cf151b355db03c7634c4647
Jan Benda <jan.benda@uni-tuebingen.de>
2021-03-17 10:18:53 +0100

    [demos] pure latex font demo

--- demos/latexfonts/latexfontsdemo.py
+++ demos/latexfonts/latexfontsdemo.py
@@ -1,10 +1,12 @@
 import os
 from collections import OrderedDict
 
+# https://tug.org/FontCatalogue
+# https://mirror.informatik.hs-fulda.de/tex-archive/macros/latex/required/psnfss/psnfss2e.pdf
+# https://tex.stackexchange.com/questions/59702/suggest-a-nice-font-family-for-my-basic-latex-template-text-and-math/59706
+
 fonts = OrderedDict()
-"""
 fonts['default'] = ''
-"""
 fonts['pslatex'] = '\\usepackage{pslatex}'
 fonts['lmodern'] = '\\usepackage{lmodern}'
 #fonts['dejavu'] = '\\usepackage{dejavu}'
@@ -17,59 +19,26 @@ fonts['mathptmx'] = '\\usepackage{mathptmx}'  # Times
 fonts['newpx'] = '\\usepackage{newpxtext}\n\\usepackage{newpxmath}'
 fonts['newtx'] = '\\usepackage{newtxtext}\n\\usepackage{newtxmath}'
 fonts['default-sf'] = '\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{sfmath}'
+fonts['default-sf-sansmath'] = '\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{sansmathfonts}'
 fonts['pslatex-sf'] = '\\usepackage{pslatex}\n\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{sfmath}'
-fonts['helvet-sansmath'] = '\\usepackage{sansmathfonts}\n\\usepackage[scaled=.92]{helvet}\n\\renewcommand{\\familydefault}{\\sfdefault}'  # Helvetica
+fonts['helvet-sansmath'] = '\\usepackage{sansmathfonts}\n\\usepackage[scaled=.92]{helvet}\n\\renewcommand{\\familydefault}{\\sfdefault}'  # Helvetica, sansmathfonts needs to be included first, sfmath is not so good
 #fonts['dejavu-sf'] = '\\usepackage{DejaVuSans-TLF}'
-fonts['bera-sf'] = '\\usepackage{berasans}\n\\renewcommand{\\familydefault}{\\sfdefault}'
-fonts['stix-sf'] = '\\usepackage{stix}\n\\renewcommand{\\familydefault}{\\sfdefault}'
+fonts['bera-sf'] = '\\usepackage{bera}\n\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{sfmath}'
+#fonts['bera-sf-lucbmath'] = '\\usepackage{bera}\n\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage[expert]{lucbmath}\n\\def\\DeclareLucidaFontShape#1#2#3#4#5#6{\\DeclareFontShape{#1}{#2}{#3}{#4}{<->s*[0.90]#5}{#6}}'  # https://rcweb.dartmouth.edu/doc/texmf-dist/doc/fonts/bera/bera.txt
 fonts['iwona'] = '\\usepackage[math]{iwona}'
+fonts['kurier'] = '\\usepackage[math]{kurier}'
 fonts['tgheros-sf'] = '\\usepackage{tgheros}\n\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{sfmath}'    
 fonts['arev'] = '\\usepackage{arev}'
-fonts['avant'] = '\\usepackage{avant}\n\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{sfmath}'
+fonts['avant'] = '\\usepackage{avant}\n\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{sansmathfonts}'
 fonts['cmbright'] = '\\usepackage{cmbright}\n\\SetSymbolFont{largesymbols}{normal}{OMX}{iwona}{m}{n}'
 fonts['lmodern-sf'] = '\\usepackage{lmodern}\n\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{sfmath}'
 fonts['kpfonts-sf'] = '\\usepackage[sfmath]{kpfonts}\n\\renewcommand{\\familydefault}{\\sfdefault}'
 fonts['newtx-sf'] = '\\usepackage{newtxtext}\n\\renewcommand{\\familydefault}{\\sfdefault}\n\\usepackage{newtxmath}\n\\usepackage{sfmath}'
 
-# https://tug.org/FontCatalogue
-# https://mirror.informatik.hs-fulda.de/tex-archive/macros/latex/required/psnfss/psnfss2e.pdf
-# https://tex.stackexchange.com/questions/59702/suggest-a-nice-font-family-for-my-basic-latex-template-text-and-math/59706
 
-def make_latex(fonts_key):
+def make_latex(secnum, fonts_key):
     fonts_str = fonts[fonts_key]
     base_name = fonts_key
-
-    print()
-    print('PLOT %s-plot.pdf' % base_name)
-
-    with open(base_name + '-plot.py', 'w') as df:
-        df.write('import numpy as np\n')
-        df.write('import matplotlib as mpl\n')
-        df.write('import matplotlib.pyplot as plt\n')
-        df.write('\n')
-        df.write('x = np.linspace(0.0, 10.0, 200)\n')
-        df.write('y = np.sin(2.0*np.pi*0.5*x)\n')
-        df.write('mpl.rcdefaults()\n')
-        df.write('plt.rcParams["font.size"] = 11\n')
-        #df.write('plt.rcParams["font.family"] = "sans-serif"\n')
-        df.write('plt.rcParams["text.usetex"] = True\n')
-        preamble = r'\usepackage[T1]{fontenc}'
-        preamble += fonts_str.replace('\n', '')
-        #preamble += r'\usepackage[warn]{textcomp}'
-        df.write('plt.rcParams["text.latex.preamble"] = r"%s"\n' % preamble)
-        df.write('fig, ax = plt.subplots(figsize=(16.0/2.54, 8.0/2.54))\n')
-        df.write('fig.subplots_adjust(bottom=0.15)\n')
-        df.write('ax.plot(x, y)\n')
-        df.write('ax.set_xlabel(r"Time [ms]")\n')
-        df.write('ax.set_ylabel(r"Current [nA]")\n')
-        df.write('fig.savefig("%s-plot.pdf")\n' % base_name)
-        df.write('plt.close()\n')
-    os.system('python3 %s-plot.py' % base_name)
-    os.remove(base_name + '-plot.py')
-
-    if not os.path.isfile(base_name + '-plot.pdf'):
-        return None
-        
     with open(base_name + '.tex', 'w') as df:
         df.write('\\documentclass[a4paper,11pt]{article}\n')
         df.write('\\usepackage[left=30mm, right=30mm, top=20mm]{geometry}\n')
@@ -81,6 +50,7 @@ def make_latex(fonts_key):
         df.write(fonts_str)
         df.write('\n')
         df.write('\\begin{document}\n')
+        df.write('\\setcounter{section}{%d}\n' % secnum)
         df.write('\\section{%s}\n' % base_name)
         df.write('\\begin{verbatim}\n')
         df.write(fonts_str)
@@ -88,23 +58,21 @@ def make_latex(fonts_key):
         df.write('\\end{verbatim}\n')
         with open('latexfonts-text.tex') as sf:
             for line in sf:
-                df.write(line.replace('IMAGEFILE', '%s-plot' % base_name))
+                df.write(line)
         df.write('\\end{document}\n')
         
     os.system('pdflatex %s' % base_name)
     os.remove(base_name + '.tex')
     os.remove(base_name + '.aux')
     os.remove(base_name + '.log')
-    os.remove(base_name + '-plot.pdf')
     return base_name
 
 
 if __name__ == "__main__":
     pdf_files = []
-    for fonts_key in fonts:
-        pdf_file = make_latex(fonts_key)
-        if pdf_file is not None:
-            pdf_files.append(pdf_file + '.pdf')
+    for k, fonts_key in enumerate(fonts):
+        pdf_file = make_latex(k, fonts_key)
+        pdf_files.append(pdf_file + '.pdf')
     os.system('pdftk ' + ' '.join(pdf_files) + ' cat output latexfontsdemo.pdf')
     # pdfjam does not work properly!
     #os.system('pdfjam --a4paper -o latexfontsdemo.pdf ' + ' '.join(pdf_files))
