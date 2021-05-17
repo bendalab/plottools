053375ae8a20f4b3a69e313a15c1353b5e15ad9e
Jan Benda <jan.benda@uni-tuebingen.de>
2021-05-17 12:29:12 +0200

    [version] added pandas

--- plottools/version.py
+++ plottools/version.py
@@ -7,13 +7,18 @@
 import sys
 import numpy as np
 import matplotlib as mpl
+try:
+    import pandas as pd
+    have_pandas = True
+except ImportError:
+    have_pandas = False
 
 
 __pdoc__ = {}
 __pdoc__['__version__'] = True
 __pdoc__['__year__'] = True
 
-__version__ = '0.7'
+__version__ = '0.8'
 """ Version of the plottools package. """
 
 __year__ = '2021'
@@ -25,6 +30,8 @@ def versions():
     """
     print('python     version: %d.%d.%d' % (sys.version_info[:3]))
     print('numpy      version:', np.__version__)
+    if have_pandas:
+        print('pandas     version:', pd.__version__)
     print('matplotlib version:', mpl.__version__)
     print('plottools  version:', __version__)
 
