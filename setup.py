609b1374b8173239a6883d3dfdce70227934da88
Jan Benda <jan.benda@uni-tuebingen.de>
2024-04-18 09:36:27 +0200

    [setup] added pyproject.toml

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
-from setuptools import setup, find_packages
-
-exec(open('plottools/version.py').read())
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
 setup(
-    name='plottools',
-    version=__version__,
-    author='Jan Benda',
-    author_email="jan.benda@uni-tuebingen.de",
-    description='Simplify production of publication-quality figures based on matplotlib.',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/bendalab/plottools",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Operating System :: OS Independent",
-    ],
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['numpy', 'matplotlib']
+    packages = ['plottools']
 )
