eab2c5a4ac0922cb4d51c707b66aaf7254e624ce
Jan Benda <jan.benda@uni-tuebingen.de>
2024-10-29 23:01:27 +0100

    [remove] new remove_style() functions

--- src/plottools/remove.py
+++ src/plottools/remove.py
@@ -4,8 +4,9 @@ Remove specific artists from an axes.
 
 ## Axes member functions
 
-- `remove_lines()`: remove all line artists without marker.
-- `remove_markers()`: remove all line artist with markers that are not connected by lines.
+- `remove_lines()`: remove all line artists.
+- `remove_markers()`: remove all line artists with markers that are not connected by lines.
+- `remove_style()`: remove all line artists that match a style.
 
 
 ## Install/uninstall remove functions
@@ -21,23 +22,19 @@ import matplotlib as mpl
 
 
 def remove_lines(ax):
-    """Remove all line artists that are lines without a marker.
+    """Remove all line artists.
 
     Parameters
     ----------
     ax: matplotlib axes
        Axes from which lines should be removed.
     """
-    remove_lines = []
     for line in ax.get_lines():
-        if line.get_marker() == 'None':
-            remove_lines.append(line)
-    for line in remove_lines:
         line.remove()
 
 
 def remove_markers(ax):
-    """Remove all line artist with markers that are not connected by lines.
+    """Remove all line artists with markers that are not connected by lines.
 
     Parameters
     ----------
@@ -47,6 +44,48 @@ def remove_markers(ax):
     remove_lines = []
     for line in ax.get_lines():
         if line.get_marker() != 'None' and line.get_linestyle() == 'None':
+            remove_lines.append(line)
+    for line in remove_lines:
+        line.remove()
+
+
+def remove_style(ax, **style):
+    """Remove all line artists that match a style.
+
+    Parameters
+    ----------
+    ax: matplotlib axes
+       Axes from which lines should be removed.
+    style: dict
+       Line style (color, linewidth, marker, makeredgecolor, etc.)
+    """
+    remove_lines = []
+    for line in ax.get_lines():
+        same = True
+        for k in style:
+            if k == 'color' and line.get_color() != style[k]:
+                same = False
+            elif k in ['linestyle', 'ls'] and line.get_linestyle() != style[k]:
+                same = False
+            elif k in ['linewidth', 'lw'] and line.get_linewidth() != style[k]:
+                same = False
+            elif k == 'marker' and line.get_marker() != style[k]:
+                same = False
+            elif k in ['markeredgecolor', 'mec'] and line.get_markeredgecolor() != style[k]:
+                same = False
+            elif k in ['markeredgewidth', 'mew'] and line.get_markeredgewidth() != style[k]:
+                same = False
+            elif k in ['markerfacecolor', 'mfc'] and line.get_markerfacecolor() != style[k]:
+                same = False
+            elif k in ['markersize', 'ms'] and line.get_markersize() != style[k]:
+                same = False
+            elif k == 'alpha' and line.get_alpha() != style[k]:
+                same = False
+            elif k == 'zorder' and line.get_zorder() != style[k]:
+                same = False
+            elif k == 'label' and line.get_label() != style[k]:
+                same = False
+        if same:
             remove_lines.append(line)
     for line in remove_lines:
         line.remove()
@@ -66,6 +105,8 @@ def install_remove():
         mpl.axes.Axes.remove_lines = remove_lines
     if not hasattr(mpl.axes.Axes, 'remove_markers'):
         mpl.axes.Axes.remove_markers = remove_markers
+    if not hasattr(mpl.axes.Axes, 'remove_style'):
+        mpl.axes.Axes.remove_style = remove_style
 
         
 def uninstall_remove():
@@ -81,6 +122,8 @@ def uninstall_remove():
         delattr(mpl.axes.Axes, 'remove_lines')
     if hasattr(mpl.axes.Axes, 'remove_markers'):
         delattr(mpl.axes.Axes, 'remove_markers')
+    if hasattr(mpl.axes.Axes, 'remove_style'):
+        delattr(mpl.axes.Axes, 'remove_style')
 
 
 install_remove()
